38d33b52d7196dba1ea644b70e7b92643c8df7a3
Alejandro <alexsotog82@gmail.com>
2020-03-13 22:35:38 -0600

    se sube curso de angular

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [ngStyle]="{'font-size': tamano + 'px'}">
+      Hola Mundo esta es una etiqueta
+    </p>
+    <p [style.fontSize.px]="tamano">
+      este es otro template
+    </p>
+    <button class="btn btn-primary" (click)="tamano = tamano + 5">
+      Mas<i class="fa fa-plus"></i>
+    </button>
+    <button class="btn btn-primary" (click)="tamano = tamano - 5">
+      Menos<i class="fa fa-minus"></i>
+    </button>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano: number = 30;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
